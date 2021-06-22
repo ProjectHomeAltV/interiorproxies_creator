2b67d75a636f3181330ab951d322d0867d766626
Bruno <39590058+Katalam@users.noreply.github.com>
2021-06-22 16:35:56 +0200

    Added ability to create interiorsproxies for altv resources

--- src/app.js
+++ src/app.js
@@ -2,53 +2,34 @@ var path = require("path");
 var glob = require('glob');
 var fs = require('fs');
 
-createFile();
 
-function createFile() {  
-    fs.writeFileSync('../output/interiorsproxies.meta', `<?xml version="1.0" encoding="UTF-8"?>\n\n<SInteriorOrderData>
+
+glob("../input/**/**.ymap", function (er, files) {
+    for (i = 0; i < files.length; i++) {
+        var raw = path.dirname(path.dirname(files[i]))
+        while (path.basename(raw) === 'assets' || path.basename(raw) === 'stream')
+            raw = path.dirname(raw)
+        var name = raw
+        var name2 = path.basename(files[i]).replace('.ymap', "")
+        if (!name2.includes('milo_')) continue;
+        if (name2.includes('gabz')) continue;
+        createInteriorProxy(name, name2)
+    }
+    setTimeout(() => {
+        process.exit();
+    }, 2000);
+})
+
+
+function createInteriorProxy(name, name2) {
+    console.log('-- ' + name + ' -- ' + name2 + ' --')
+    fs.writeFileSync('../input/' + name + '/stream/interiorsproxies.meta', `<?xml version="1.0" encoding="UTF-8"?>\n\n<SInteriorOrderData>
     <startFrom value="2000" />
     <filePathHash value="0" />
     <proxies>
-    `);
-
-    appendYTD()
-}
-
-function appendYTD() {
-    glob("../input/**/**.ymap", function (er, files) {
-        let ii = 0;
-        for (i=0; i < files.length; i++) {
-            var name = path.basename(files[i])
-            var name2 = name.replace('.ymap', "")
-            if (!name.includes('milo_')) continue;
-            fs.appendFile('../output/interiorsproxies.meta', (ii == 0 ? "  ".repeat(2) : "  ".repeat(4)) + '<Item>' + name2 + '</Item>\n', function (err) {
-                if (err) {
-                    console.log('An error occured.');
-                } else {
-                    return;
-                }
-            });
-
-            if (i == files.length-1) {
-                setTimeout(() => {
-                    fs.appendFile('../output/interiorsproxies.meta', "  ".repeat(2) + '</proxies>\n</SInteriorOrderData>', function(err) {
-                        if (err) {
-                            console.log('An error occured.');
-                        } else {
-                            return;
-                        }
-                    });
-                }, 1000);
-            }
-
-            ii += 1;
-            console.log(name2)
-        }
-
-        console.log('\nA resource has been created at ../output/interiorsproxies.meta')
-
-        setTimeout(() => {
-            process.exit();
-        }, 2000);
-    })
+    `, function () {});
+    fs.appendFile('../input/' + name + '/stream/interiorsproxies.meta', "  ".repeat(2) + '<Item>' + name2 + '</Item>\n', function () {});
+    setTimeout(() => {
+        fs.appendFile('../input/' + name + '/stream/interiorsproxies.meta', "  ".repeat(2) + '</proxies>\n</SInteriorOrderData>', function () {});
+    }, 1000);
 }
